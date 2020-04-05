8e357482152e40c1dbe705614bbe9e7fd34de50b
Faizan <fmouhamad@hotmail.fr>
2020-04-05 15:01:07 +0200

    configure routes

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Dashboard } from "./components/Dashboard/Dashboard";
+import { Login } from "./components/Login/Login";
+import { Signup } from "./components/Signup/Signup";
+import { PrivateRoute } from "./components/PrivateRoute";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-content">
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <PrivateRoute path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
